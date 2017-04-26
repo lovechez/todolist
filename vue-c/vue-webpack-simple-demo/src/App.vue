fdfcc2974466847d42e39b9931a21a00d70b752c
lovechez <lovechez>
2017-04-26 15:08:36 +0800

    cli

---
+++ vue-c/vue-webpack-simple-demo/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+    <input type="button" value="change" @click="change">
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Hello Vue1!'
+    }
+  },
+  methods:{
+    change(){
+      this.msg= this.msg.split('').reverse().join('');;
+    }
+  }
+
+}
+</script>
+
+<style>
+body {
+  font-family: Helvetica, sans-serif;
+}
+</style>
